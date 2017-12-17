793bc5c959b01dbf68f8e11791ebd88b3aaf05b1
wh52 <cp.vlv.el.52wtr@gmail.com>
2017-12-18 00:51:56 +0900

    update model8-2

--- rstan2edward/model8-2.py
+++ rstan2edward/model8-2.py
@@ -10,15 +10,30 @@ import seaborn as sns
 from edward.models import Normal, Empirical, Uniform
 
 ed.set_seed(42)
-data = pd.read_csv("../data/data-salary-2.txt")
+data = pd.read_csv("data/data-salary-2.txt")
+# T=5000
+N = len(data)
 
-X = tf.placeholder(tf.float32, [None])
-KID = tf.placeholder(tf.float32, [None])
-a = Uniform(low=-1000, high=1000)
-b = Uniform(low=-1000, high=1000)
-sd = Uniform(low=0, high=1000)
-y = Normal(loc=a*KID+b*KID*X, scale=sd)
+# model
+X = tf.placeholder(tf.float32, [N])
+KID = Uniform(low=0.0, high=4.0, sample_shape=N)
+a = Uniform(low=-1000.0, high=1000.0, sample_shape=N)
+b = Uniform(low=-1000.0, high=1000.0, sample_shape=N)
+sd = Uniform(low=0.0, high=1000.0, sample_shape=N)
+Y = Normal(loc=a*KID+b*KID*X, scale=sd, sample_shape=N)
 
-inference = ed.KLqp({a:qa, b:qb, }, data={X:data['X'], KID:data['KID'], y:data['Y']})
+# inference
+qa = Empirical(params=tf.Variable(tf.random_normal([N])))
+qb = Empirical(params=tf.Variable(tf.random_normal([N])))
+qsd = Empirical(params=tf.Variable(tf.random_normal([N])))
+
+# preprocess
+x = np.array(data['X']).reshape((N,1))
+kid = np.array(data['KID']).reshape((N, 1))
+y = np.array(data['Y']).reshape((N, 1))
+
+# variational inference
+inference = ed.KLqp({a:qa, b:qb, sd:qsd}, data={X:x, KID:kid, Y:y})
+inference = tf_debug.LocalCLIDebugWrapperSession(inference)
 inference.run()
 
