e8486f2e37c6153d0160812ab375b3926a4bfd82
wh52 <cp.vlv.el.52wtr@gmail.com>
2017-12-20 17:44:15 +0900

    update model8-2.py

--- rstan2edward/model8-2.py
+++ rstan2edward/model8-2.py
@@ -11,29 +11,26 @@ from edward.models import Normal, Empirical, Uniform
 
 ed.set_seed(42)
 data = pd.read_csv("data/data-salary-2.txt")
-# T=5000
+T = 3000
 N = len(data)
 
 # model
 X = tf.placeholder(tf.float32, [N])
-KID = Uniform(low=0.0, high=4.0, sample_shape=N)
-a = Uniform(low=-1000.0, high=1000.0, sample_shape=N)
-b = Uniform(low=-1000.0, high=1000.0, sample_shape=N)
-sd = Uniform(low=0.0, high=1000.0, sample_shape=N)
-Y = Normal(loc=a*KID+b*KID*X, scale=sd, sample_shape=N)
+KID = tf.placeholder(tf.float32, [N])
+a = Uniform(low=-1000.0, high=1000.0)
+b = Uniform(low=-1000.0, high=1000.0)
+sd = Uniform(low=0.0, high=1000.0)
+Y = Normal(loc=a*KID+b*KID*X, scale=sd)
 
 # inference
-qa = Empirical(params=tf.Variable(tf.random_normal([N])))
-qb = Empirical(params=tf.Variable(tf.random_normal([N])))
-qsd = Empirical(params=tf.Variable(tf.random_normal([N])))
+qa = Empirical(params=tf.Variable(tf.random_normal([T])))
+qb = Empirical(params=tf.Variable(tf.random_normal([T])))
+qsd = Empirical(params=tf.Variable(tf.random_normal([T])))
 
 # preprocess
-x = np.array(data['X']).reshape((N,1))
-kid = np.array(data['KID']).reshape((N, 1))
-y = np.array(data['Y']).reshape((N, 1))
-
-# variational inference
-inference = ed.KLqp({a:qa, b:qb, sd:qsd}, data={X:x, KID:kid, Y:y})
-inference = tf_debug.LocalCLIDebugWrapperSession(inference)
+x = np.array(data['X'])
+kid = np.array(data['KID'])
+y = np.array(data['Y'])
+# Hamiltonian Monte Carlo
+inference = ed.HMC({a:qa, b:qb, sd:qsd}, data={X:x, KID:kid, Y:y})
 inference.run()
-
